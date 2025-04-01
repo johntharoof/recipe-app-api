7ef8930ae358928422d5bce7ad0554db4e16e99d
John <john.veronelli@icloud.com>
2025-04-01 12:08:47 +0100

    adds tests and superuser creation function

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -37,3 +37,13 @@ class ModelTests(TestCase):
         """Test creating user without email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
